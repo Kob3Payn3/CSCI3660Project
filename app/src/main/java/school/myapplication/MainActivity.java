2513cc43efe4936f58e1f28c88cfda33120f908f
Kob3Payn3 <kobepayne1@gmail.com>
2024-04-23 14:14:12 -0400

    the buttons appear at random time intervals set by the delay in shuffleButtons method, had to had a boolean to check if shuffling should occur because sometimes the button would appear after the endgame dialog.

--- app/src/main/java/school/myapplication/MainActivity.java
+++ app/src/main/java/school/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView timerTextView;
     private CountDownTimer countDownTimer;
     long timerLength = 20000;
+    private boolean shuffleEnabled = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
             moleHoleButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+                    v.setEnabled(false);
                     // Increase the score by 10 and shuffle buttons
                     game.whackingTheMole();
                     shuffleButtons();
@@ -136,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         disableMoleButtons();
         showEndGameDialog(game.getPlayerScore());
         game.resetPlayerScore();
-
+        shuffleEnabled = false;
         // Enable settings button after game end
         Button settingsButton = findViewById(R.id.timer_options_button);
         settingsButton.setEnabled(true);
@@ -167,10 +172,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void shuffleButtons() {
-        // Randomize and enable buttons
-        ButtonRandomizer buttonRandomizer = new ButtonRandomizer();
-        List<Integer> enabledButtonIds = buttonRandomizer.getRandomButtonIds(1);
-        enableSelectedButtons(enabledButtonIds);
+        // Checks to see if shuffling is allowed
+        if (!shuffleEnabled) {
+            return;
+        }
+
+        Random random = new Random();
+        // Button enabled at random time intervals in this range
+        int delay = random.nextInt(500) + 500;
+        // Randomize and enable buttons, the shuffling and enabling of buttons occurs at random intervals set by the delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ButtonRandomizer buttonRandomizer = new ButtonRandomizer();
+                List<Integer> enabledButtonIds = buttonRandomizer.getRandomButtonIds(1);
+                enableSelectedButtons(enabledButtonIds);
+            }
+        }, delay);
     }
 
     private void enableSelectedButtons(List<Integer> enabledButtonIds) {
